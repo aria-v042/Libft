fead91c2b3c281012d775855c1d4f54483ac4ae4
aria <redcore_one@proton.me>
2026-04-22 17:01:00 +0100

    test: add test.c for testing functions

---
+++ test.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/21 22:13:37 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/04/22 16:17:36 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void	test_ft_isalpha(char c, int exp)
+{
+	printf("Testing ft_isalpha()...\n");
+	printf("> ft_isalpha(%d) ? %d\n", c, exp);
+	if (ft_isalpha(c) == exp)
+		printf(">>> PASSED\n");
+	else
+		printf("<<< FAILED\n");
+}
+
+int	main(int argc, char *argv)
+{
+	test_ft_isalpha(a, 1);
+}
