828ced580dc6e142c8327a9d054ca5a4e394975e
aria <redcore_one@proton.me>
2026-04-23 16:46:50 +0100

    test: add new tests for ft_isalpha()

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 22:13:37 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/04/22 22:54:45 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/04/23 16:45:32 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,25 @@
 
 void	test_ft_isalpha(char c, int exp)
 {
-	printf("Testing ft_isalpha()...\n");
-	printf("> ft_isalpha(%c) ? %d\n", c, exp);
+	printf("> ft_isalpha('%c') ? %d\n", c, exp);
 	if (ft_isalpha(c) == exp)
-		printf(">>> PASSED\n");
+		printf(">>> PASSED <<<\n");
 	else
-		printf("<<< FAILED\n");
+		printf("<<< FAILED >>>\n");
 }
 
 int	main(int argc, char **argv)
 {
+	printf("Testing ft_isalpha()...\n\n");
 	test_ft_isalpha('a', 1);
+	printf("\n");
+	test_ft_isalpha('9', 0);
+	printf("\n");
+	test_ft_isalpha(' ', 0);
+	printf("\n");
+	test_ft_isalpha('Z', 1);
+	printf("\n");
+	test_ft_isalpha('\t', 0);
+	printf("\n");
 	return (1);
 }
