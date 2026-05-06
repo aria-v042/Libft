4b5af3aa5f9e5f1723057f9e90a8bc3283727ac9
aria <redcore_one@proton.me>
2026-05-06 01:56:01 +0100

    memmove: add ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 00:35:38 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/05/06 01:55:01 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*aux;
+
+	while (n-- > 0)
+	{
+		*(aux + n) = *((unsigned char *)src + n);
+	}
+	return (dest);
+}
