7b8fba1e026456dbc7734deebff0acc169cb6f36
aria <redcore_one@proton.me>
2026-05-10 18:17:01 +0100

    memmove: update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,12 +14,10 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*aux = (unsigned char *)malloc(n);
+	unsigned char	*aux;
 
-	while (n-- > 0)
-		*(aux + n) = *((unsigned char *)src + n);
-	while ()
-
-	
+	aux = (unsigned char *)malloc(n);
+	ft_memcpy(aux, src, n);
+	ft_memcpy(dest, aux, n);
 	return (dest);
 }
