1a04029eae2ba411db90afd093e77bda4f9914b4
aria <redcore_one@proton.me>
2026-05-10 22:32:30 +0100

    strlcpy: update variable names for clarity

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,26 +6,26 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 18:31:42 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/10 22:18:38 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/10 22:31:43 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
 {
-	size_t	len;
+	size_t	src_len;
 
-	len = ft_strlen(src);
-	if (len < size)
+	src_len = ft_strlen(src);
+	if (src_len < dst_size)
 	{
-		ft_memcpy(dst, src, len);
-		dst[len] = 0;
+		ft_memcpy(dst, src, src_len);
+		dst[src_len] = 0;
 	}
-	else if (size)
+	else if (dst_size)
 	{
-		ft_memcpy(dst, src, size - 1);
-		dst[size - 1] = 0;
+		ft_memcpy(dst, src, dst_size - 1);
+		dst[dst_size - 1] = 0;
 	}
-	return (len);
+	return (src_len);
 }
