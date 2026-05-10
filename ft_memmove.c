47cbb92d0e581f73cd502920ecf454267d41d2ac
aria <redcore_one@proton.me>
2026-05-10 18:59:06 +0100

    memmove: change aux buffer to static type

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 00:35:38 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/06 01:55:01 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/10 18:57:34 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*aux;
+	static unsigned char	*aux;
 
 	aux = (unsigned char *)malloc(n);
 	ft_memcpy(aux, src, n);
