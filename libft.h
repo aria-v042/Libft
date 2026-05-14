669193a82168720a1b080b5810b6f224d3e3a4d9
aria <redcore_one@proton.me>
2026-05-14 20:57:35 +0100

    strchr: add ft_strchr declaration to header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 22:23:55 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/14 20:30:17 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/14 20:43:54 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
