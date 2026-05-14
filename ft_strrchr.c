1e8501f7fb4cf74fdc4aa1a08b3dedfe5ea2a85c
aria <redcore_one@proton.me>
2026-05-14 22:10:17 +0100

    strrchr: add cast to return pointer

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 20:44:55 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/14 21:47:52 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/14 22:09:28 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ char	*ft_strrchr(const char *s, int c)
 	while (i && (s[i] != c))
 		i--;
 	if (s[i] == c)
-		return (&s[i]);
+		return ((char *)&s[i]);
 	return (NULL);
 }
