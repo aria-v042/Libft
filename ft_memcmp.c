631fc8b7714d95950da0b14723f3dfae597d8891
aria <redcore_one@proton.me>
2026-05-16 00:28:57 +0100

    memcmp: fix norm in implementation

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,19 +6,19 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 00:00:04 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/16 00:14:23 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/16 00:28:35 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char	*b1;
 	const unsigned char	*b2;
-	
-	b1 = (const unsigned char	*)s1;
-	b2 = (const unsigned char	*)s2;
+
+	b1 = (const unsigned char *)s1;
+	b2 = (const unsigned char *)s2;
 	while (n--)
 	{
 		if (*b1 != *b2)
