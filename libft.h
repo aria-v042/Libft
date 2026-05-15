4ce5496875393ba703a2853b6eac034e45f6282b
aria <redcore_one@proton.me>
2026-05-16 00:32:47 +0100

    strnstr: add ft_strnstr declaration to header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 22:23:55 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/16 00:01:00 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/16 00:31:55 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const char *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
