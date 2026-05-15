5639f391ab54825cb0f723e4df698a60979886d5
aria <redcore_one@proton.me>
2026-05-16 00:15:58 +0100

    memcmp: add ft_memcmp implementation

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/16 00:00:04 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/05/16 00:14:23 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*b1;
+	const unsigned char	*b2;
+	
+	b1 = (const unsigned char	*)s1;
+	b2 = (const unsigned char	*)s2;
+	while (n--)
+	{
+		if (*b1 != *b2)
+			return (*b1 - *b2);
+		b1++;
+		b2++;
+	}
+	return (0);
+}
