70211a09425ff94280426acb1f02ef22e375368a
aria <redcore_one@proton.me>
2026-05-15 22:29:10 +0100

    strncmp: rewrite ft_strncmp implementation

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,12 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
+	while (n && *s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+		n--;
+	}
 	if (!n)
 		return (0);
-	while (s1[i] && s2[i] && ((unsigned char) s1[i] != (unsigned char) s2[i]) && i < n)
-		i++;
-	return (s1[i] - s2[i]);
+	else
+	{
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	}
 }
