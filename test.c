f52e67023aa319b202e07ccd4ba3069bb347ad13
aria <redcore_one@proton.me>
2026-05-16 14:56:49 +0100

    test: add ANSI color codes for styling output

--- test.c
+++ test.c
@@ -15,6 +15,14 @@
 #include <string.h>
 #include <stdarg.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 static int	tested = 0;
 static int	passed = 0;
 static int	failed = 0;
