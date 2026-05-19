979f3855f5da3efa54244aeea31701a5cb180d92
aria <redcore_one@proton.me>
2026-05-19 19:55:26 +0100

    header/strtrim: add ft_strtrim function declaration

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 22:23:55 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/19 19:49:34 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/19 19:54:57 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ char	*ft_strdup(const char *s);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
