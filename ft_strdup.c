a0194ec0146dc74438613c9895332fce660062cc
aria <redcore_one@proton.me>
2026-05-19 01:27:35 +0100

    ft/strdup: add ft_strdup implementation

---
+++ ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 01:10:39 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/05/19 01:26:12 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	size;
+
+	size = ft_strlen(s) + 1;
+	dup = (char *)malloc(size * sizeof(char));
+	if (!dup)
+		return (NULL);
+	ft_strlcpy(dup, s, size);
+	return (dup);
+}
