cf06b01d9743905ff49731911dff8e11e21a8866
aria <redcore_one@proton.me>
2026-05-19 03:30:43 +0100

    ft/substr: add ft_substr implementation

---
+++ ft_substr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 02:37:55 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/05/19 02:37:55 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dup;
+
+	dup = ft_calloc((len + 1, sizeof(char));
+	if (!dup)
+		return (NULL);
+	if (start <= ft_strlen(s) - 1)
+		ft_strlcpy(dup, s + start, len);
+	return (dup);
+}
