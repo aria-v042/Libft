088fea9b8f8f29340f9113ff6fb0ca3fbfcce11a
aria <redcore_one@proton.me>
2026-05-21 02:11:06 +0100

    ft/strmapi: update implementation

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 01:47:09 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/21 02:05:13 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/21 02:10:45 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char	*s2;
+	char			*s2;
+	unsigned int	i;
 
 	s2 = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
 	if (!s2)
 		return (NULL);
-	while (s)
-		*s2 = f(s, *(s++));
+	i = 0;
+	while (s[i])
+		s2[i] = f(i, s[i++]);
 	return (s2);
 }
