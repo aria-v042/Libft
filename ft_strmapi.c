2266190ae6bb3c08188eef4316b6855a347a4c70
aria <redcore_one@proton.me>
2026-05-21 02:23:54 +0100

    ft/strmapi: fix bugs in implementation

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 01:47:09 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/21 02:10:45 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/21 02:22:45 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char			*s2;
-	unsigned int	i;
+	char	*s2;
+	size_t	i;
 
+	if (!s || !f)
+		return (NULL);
 	s2 = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
 	if (!s2)
 		return (NULL);
 	i = 0;
 	while (s[i])
-		s2[i] = f(i, s[i++]);
+	{
+		s2[i] = f(i, s[i]);
+		i++;
+	}
+	s2[i] = '\0';
 	return (s2);
 }
