4929673adce16d96c726e3e0f15af2100ff27427
aria <redcore_one@proton.me>
2026-05-22 00:33:16 +0100

    ft/strmapi(style): remove redundant 'sizeof(char)'

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -19,7 +19,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 
 	if (!s || !f)
 		return (NULL);
-	s2 = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	s2 = (char *)malloc(ft_strlen(s) + 1);
 	if (!s2)
 		return (NULL);
 	i = 0;
