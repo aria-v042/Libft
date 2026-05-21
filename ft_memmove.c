5d5fe435d89517f1558332333c94c875c1edfacf
aria <redcore_one@proton.me>
2026-05-21 18:29:26 +0100

    ft/memmove: reimplement using pointer arithmetic

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,13 +14,18 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*aux;
+	unsigned char		*d;
+	const unsigned char	*s;
 
-	if (!dest && !src)
-		return (NULL);
-	aux = (unsigned char *)malloc(n);
-	ft_memcpy(aux, src, n);
-	ft_memcpy(dest, aux, n);
-	free(aux);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d == s || n == 0)
+		return (dest);
+	if (d < s)
+		while (n--)
+			*d++ = *s++;
+	else
+		while (n--)
+			*(d + n) = *(s + n);
 	return (dest);
 }
