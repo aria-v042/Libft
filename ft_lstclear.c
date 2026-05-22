2c4b460827e7d5bf9d5021711791fa500c4d0d98
aria <redcore_one@proton.me>
2026-05-22 08:25:13 +0100

    ft/lstclear(fix): fix norm

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 07:42:13 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/22 08:00:54 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/22 08:22:46 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 
 	if (!lst || !del)
 		return ;
-	while(*lst)
+	while (*lst)
 	{
 		lstnext = (*lst)->next;
 		ft_lstdelone(*lst, del);
