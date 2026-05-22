068e3a5369ddc12b4cdbc4fd76245993202e6d45
aria <redcore_one@proton.me>
2026-05-22 05:21:38 +0100

    ft/lstadd_front(fix): fix function data type

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 04:38:35 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/22 05:09:58 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/22 05:14:56 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!new)
 		return ;
