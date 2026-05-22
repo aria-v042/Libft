1416e7aee74c5b1434238d1df758f6c471f15bac
aria <redcore_one@proton.me>
2026-05-22 10:26:14 +0100

    ft/memcpy(add): guard against case where dest and src are null to pass dumb test

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,9 +17,35 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
+	if (!dest && !src)
+		return (NULL);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
 	while (n--)
 		*d++ = *s++;
 	return (dest);
 }
+//
+///* DEBUGGING */
+//
+//#include <string.h>
+//
+//int	main(void)
+//{
+//	char	src[] = "test overlap";
+//	char	*dest;
+//	char	*res;
+//
+//	// Test overlap
+//	dest = src + 1
+//	res = memcpy(dest, src, 1);
+//	printf("%s\n", res);
+//
+//	// Test case where dest and src null but size is not zero
+//	dest = NULL;
+//	src = NULL;
+//	dest = memcpy(dest, src, 1);
+//	printf("%s\n", dest);
+//
+//	return (0);
+//}
